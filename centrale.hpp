9dadd74121ba1d5709c6cf32a1e3bba97e3f9c40
unknown <ingenieur@DESKTOP-MTE998H.univ-paris13.fr>
2021-04-10 18:11:07 +0200

    add

--- centrale.hpp
+++ centrale.hpp
@@ -9,29 +9,36 @@ class centrale
 {
     public:
     centrale();
-    centrale(double etat_enceinte,double pression_enceinte,double radioactivite,double etat_centrale,
-             double production_centrale);
+  /*  centrale(double etat_enceinte,double pression_enceinte,double radioactivite,double etat_centrale,
+             double production_centrale);*/
 
+    ~centrale()= default;
     double get_etat_enceinte()const;
     double get_pression_enceinte()const;
     double get_radioactivite()const;//Radioactivité de l’enceinte de confinement à l’intérieur(non)Notez que si l’enceinte est abîmée, la radioactivité baisse dans l’enceinte (mais pas à l’extérieur!)
     double get_etat_centrale()const;
     double get_production_centrale()const;
+    circuit_prim get_circuitprim()const;
+    circuit_sec get_circuitsec()const;
+    reacteur get_reacteur()const;
 
     void set_etat_enceinte( const double valeur_demandee);
     void set_pression_enceinte( const double valeur_demandee);
     void set_radioactivite(const double valeur_demandee);//Radioactivité de l’enceinte de confinement à l’intérieur(non)Notez que si l’enceinte est abîmée, la radioactivité baisse dans l’enceinte (mais pas à l’extérieur!)
     void set_etat_centrale(const double valeur_demandee);
     void set_production_centrale(const double valeur_demandee);
+    void set_circuitprim(const circuit_prim circuitprim_demande);
+    void set_circuitsec(const circuit_sec circuitsec_demande);
+    void set_reacteur(const reacteur reacteur_demande);
 
 
     void equ_etat_enceinte();
-    void equ_pression_enceinte(const circuit_prim &circuitprim, const reacteur& reacteur, const circuit_sec &circuitsec);
-    void equ_radioactivite_enceinte(const circuit_prim & circuitprim, const reacteur& reacteur);
-    void equ_etat_centrale(const circuit_prim & circuitprim, const reacteur& reacteur, const circuit_sec & circuitsec);
-    void equ_production(const circuit_sec & circuitsec);
+    void equ_pression_enceinte();
+    void equ_radioactivite_enceinte();
+    void equ_etat_centrale();
+    void equ_production();
 
-
+    centrale& operator = (const centrale&) = default;
 
 
     private:
@@ -41,6 +48,9 @@ class centrale
       double  radioactivite;//Radioactivité de l’enceinte de confinement à l’intérieur(non)Notez que si l’enceinte est abîmée, la radioactivité baisse dans l’enceinte (mais pas à l’extérieur!)
       double etat_centrale;//État général de la centrale(non)
       double production_centrale;//Production de la centrale(non)
+      circuit_sec circuitsec;
+      circuit_prim circuitprim;
+      reacteur reacteur;
 
 
 #endif
